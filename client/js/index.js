09f1f2bf7ffeb1867167f59040d48bcd5b588db2
Fadly Kayo <fadly.kayo@googlemail.com>
2017-04-06 20:29:31 +0700

    WIP 3

--- client/js/index.js
+++ client/js/index.js
@@ -6,9 +6,25 @@ $(document).ready(function(){
   })
 });
 
+var map = new GMaps({
+  el: '#map',
+  lat: -6.2099505,
+  lng: 106.7521659
+});
+
+map.addMarker({
+  lat: -6.2099505,
+  lng: 106.7521659,
+  title: 'KIN+',
+  infoWindow: {
+    content: '<p>HTML Content</p>'
+  }
+});
+
 
 /*
 TODO:
 responsive
 javascript button
+google map api
 */
